5bf529cf029b19b0e064ccae1a974d669651f87a
Hoper-J <hoper.hw@gmail.com>
2022-09-26 19:26:45 +0800

    Fix translation issues.

--- function/translator.py
+++ function/translator.py
@@ -1,7 +1,8 @@
+import hashlib
 import requests
-
+import random
+import time
 from .baidu_translate import Dict
-
 
 class Translator:
     """
@@ -12,7 +13,7 @@ class Translator:
 
     def __init__(self):
         self.res = None
-        self.baidu_dict = Dict()  # 只获取一次html源码中的参数，加快翻译速度，如果每次都获取，速度差不多只有1/3
+        # self.baidu_dict = Dict()  # 只获取一次html源码中的参数，加快翻译速度，如果每次都获取，速度差不多只有1/3
 
     @property
     def en_to_zh(self):
@@ -23,22 +24,45 @@ class Translator:
     def en_to_zh(self, string=' '):
         """翻译字符串"""
         self.raw_string = string
-        self.__baidu_translation()
-        # self.__youdao_translation()
+        # self.__baidu_translation() # 暂时有bug
+        self.__youdao_translation()
 
     def __youdao_translation(self) -> str:
         """
-        有道翻译，未反爬虫版，不建议使用
+        有道翻译
         """
+        lts = str(int(time.time() * 1000))
+        salt = lts + str(random.randint(0, 9))
+        i = self.raw_string
+        sign_str = 'fanyideskweb' + i + salt + 'Ygy_4c=r#e#4EX^NUGUc5'
+        m = hashlib.md5()
+        m.update(sign_str.encode())
+        sign = m.hexdigest()
         # print(self.raw_string)
+        headers = {
+            "Referer": "https://fanyi.youdao.com/",
+            "Cookie": 'OUTFOX_SEARCH_USER_ID=-1124603977@10.108.162.139; JSESSIONID=aaamH0NjhkDAeAV9d28-x; OUTFOX_SEARCH_USER_ID_NCOO=1827884489.6445506; fanyi-ad-id=305426; fanyi-ad-closed=1; ___rl__test__cookies=1649216072438',
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.84 Safari/537.36"
+        }
         data = {
             'doctype': 'json',
             'type': 'AUTO',
-            'i': self.raw_string,
+            'i': i,
+            "from": "AUTO",
+            "to": "AUTO",
+            "smartresult": "dict",
+            "client": "fanyideskweb",
+            "salt": salt,
+            "sign": sign,
+            "lts": lts,
+            "bv": "a0d7903aeead729d96af5ac89c04d48e",
+            "version": "2.1",
+            "keyfrom": "fanyi.web",
+            "action": "FY_BY_REALTlME",
         }
-        url = "http://fanyi.youdao.com/translate"
+        url = "https://fanyi.youdao.com/translate_o?smartresult=dict&smartresult=rule"
         try:
-            r = requests.get(url, params=data)
+            r = requests.post(url, headers=headers, data=data)
             result = r.json()
             # 注意translateResult返回的列表长度是根据翻译的行数而定
             self.res = self.__deal_with_yd_translation(result)
